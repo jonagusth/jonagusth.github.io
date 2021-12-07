4083b3d08b274a99c30cc2fb263d2cd40fd49d3d
jonagusth <jonagusth@gmail.com>
2021-12-07 22:32:16 +0100

    allskonar

--- src/components/LordOfTheRings.vue
+++ src/components/LordOfTheRings.vue
@@ -56,13 +56,46 @@
         thematic universe. From this site, lists of characters ,appearing in
         each of the thee movies, were collected. Using this list of characters ,
         the wiki page for every character appearing in the movies were
-        collected. Each wiki page contains refrences to other characters.
+        collected. Each wiki page contains refrences to other characters. In total
+        121 wiki pages were collected, amounting to 1,24MB of data.
       </p>
       <p>
         This data was used to generate a network of the characters. Each
         character is modeled as a node in the network, and each refrence is
         modeled as a link in the network. The network constructed has 121 nodes
         and 710 nodes.
+      </p>
+      <h4>The Books</h4>
+      <p>The books were used as a data source yes</p>
+
+      <h4>The movie scripts (Transcripts)</h4>
+      <p>
+        Transcripts of the three movies were used a data sources as well. As the transcripts specify
+        very well which character is saying what, they were used to perform sentimental analysis for major
+        characters, races and cultures. The transcripts were also used to generate word clouds of frequently 
+        used words in the films.
+
+      </p>
+    </div>
+  </div>
+  <div class="row align-items-center p-3">
+    <div class="col-md-12">
+      <h2>The network</h2>
+    </div>
+    <div class="col-md-12">
+      <img src="../assets/LOTR/placeholder.png" class="img-fluid" />
+    </div>
+    <div class="col-md-12">
+      <h4>Tools used</h4>
+      <p>
+        To model the network, the python package <a href="https://networkx.org/">Networkx</a> was used.
+        This package offers many functionaniltie to analyze the network. To visualize the network, <a href="https://github.com/bhargavchippada/forceatlas2">Force atlas 2</a>
+        was use to set the positions of the nodes and <a href="http://docs.bokeh.org/en/latest/">bokeh</a> was used to generate an interactive visualisation of the network.
+      
+      </p>
+      <h4>Statistics</h4>
+      <p>Once the network was constructed, some general statistics were extracted. The constructed network
+        has 121 nodes, with 710 edges. 
       </p>
     </div>
   </div>
@@ -71,292 +104,169 @@
       <h2>A title for a chapter</h2>
     </div>
     <div class="col-md-12">
-      <img src="../assets/LOTR/placeholder.png" class="img-fluid" />
-    </div>
-    <!-- <div class="col-md-12">
       <div
         id="carouselExampleIndicators"
-        class="carousel carousel-dark slidee"
+        class="carousel carousel-dark slide"
         data-bs-ride="carousel"
       >
-        <ol class="carousel-indicators">
-          <li
+        <div class="carousel-indicators">
+          <button
+            type="button"
             data-bs-target="#carouselExampleIndicators"
             data-bs-slide-to="0"
             class="active"
-          ></li>
-          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
-          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
-          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"></li>
-          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4"></li>
-          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5"></li>
-          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6"></li>
-          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7"></li>
-          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="8"></li>
-          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="9"></li>
-          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="10"></li>
-        </ol>
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="3"
+            aria-label="Slide 4"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="4"
+            aria-label="Slide 5"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="5"
+            aria-label="Slide 6"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="6"
+            aria-label="Slide 7"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="7"
+            aria-label="Slide 8"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="8"
+            aria-label="Slide 9"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="9"
+            aria-label="Slide 10"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="10"
+            aria-label="Slide 11"
+          ></button>
+        </div>
         <div class="carousel-inner">
           <div class="carousel-item active">
             <img
-              class="d-block w-100"
               src="../assets/LOTR/characterWordClouds/aragorn.png"
-              alt="Aragorn Word Cloud"
+              class="d-block w-100"
             />
           </div>
           <div class="carousel-item">
             <img
-              class="d-block w-100"
               src="../assets/LOTR/characterWordClouds/boromir.png"
-              alt="Boromir Word Cloud"
+              class="d-block w-100"
             />
           </div>
           <div class="carousel-item">
             <img
-              class="d-block w-100"
               src="../assets/LOTR/characterWordClouds/frodo.png"
-              alt="Frodo Word Cloud"
+              class="d-block w-100"
             />
           </div>
           <div class="carousel-item">
             <img
-              class="d-block w-100"
               src="../assets/LOTR/characterWordClouds/gandalf.png"
-              alt="Gandalf Word Cloud"
+              class="d-block w-100"
             />
           </div>
           <div class="carousel-item">
             <img
-              class="d-block w-100"
               src="../assets/LOTR/characterWordClouds/gimli.png"
-              alt="Gimli Word Cloud"
+              class="d-block w-100"
             />
           </div>
           <div class="carousel-item">
             <img
-              class="d-block w-100"
               src="../assets/LOTR/characterWordClouds/gollum.png"
-              alt="Gollum Word Cloud"
+              class="d-block w-100"
             />
           </div>
           <div class="carousel-item">
             <img
-              class="d-block w-100"
               src="../assets/LOTR/characterWordClouds/legolas.png"
-              alt="Legolas Word Cloud"
+              class="d-block w-100"
             />
           </div>
           <div class="carousel-item">
             <img
-              class="d-block w-100"
               src="../assets/LOTR/characterWordClouds/merry.png"
-              alt="Merry Word Cloud"
+              class="d-block w-100"
             />
           </div>
           <div class="carousel-item">
             <img
-              class="d-block w-100"
               src="../assets/LOTR/characterWordClouds/pippin.png"
-              alt="Pppin Word Cloud"
+              class="d-block w-100"
             />
           </div>
           <div class="carousel-item">
             <img
-              class="d-block w-100"
               src="../assets/LOTR/characterWordClouds/sam.png"
-              alt="Sam Word Cloud"
+              class="d-block w-100"
             />
           </div>
           <div class="carousel-item">
             <img
-              class="d-block w-100"
               src="../assets/LOTR/characterWordClouds/saruman.png"
-              alt="Saruman Word Cloud"
+              class="d-block w-100"
             />
           </div>
         </div>
-        <a
+        <button
           class="carousel-control-prev"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
         >
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
           class="carousel-control-next"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
         >
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
-    </div> -->
-    <div
-          id="carouselExampleIndicators"
-          class="carousel carousel-dark slide"
-          data-bs-ride="carousel"
-        >
-          <div class="carousel-indicators">
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="0"
-              class="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="2"
-              aria-label="Slide 3"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="3"
-              aria-label="Slide 4"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="4"
-              aria-label="Slide 5"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="5"
-              aria-label="Slide 6"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="6"
-              aria-label="Slide 7"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="7"
-              aria-label="Slide 8"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="8"
-              aria-label="Slide 9"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="9"
-              aria-label="Slide 10"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="10"
-              aria-label="Slide 11"
-            ></button>
-          </div>
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img
-                src="../assets/LOTR/characterWordClouds/aragorn.png"
-                class="d-block w-100"
-              />
-            </div>
-            <div class="carousel-item">
-              <img
-                src="../assets/LOTR/characterWordClouds/boromir.png"
-                class="d-block w-100"
-              />
-            </div>
-            <div class="carousel-item">
-              <img
-                src="../assets/LOTR/characterWordClouds/frodo.png"
-                class="d-block w-100"
-              />
-            </div>
-            <div class="carousel-item">
-              <img
-                src="../assets/LOTR/characterWordClouds/gandalf.png"
-                class="d-block w-100"
-              />
-            </div>
-            <div class="carousel-item">
-              <img
-                src="../assets/LOTR/characterWordClouds/gimli.png"
-                class="d-block w-100"
-              />
-            </div>
-            <div class="carousel-item">
-              <img
-                src="../assets/LOTR/characterWordClouds/gollum.png"
-                class="d-block w-100"
-              />
-            </div>
-            <div class="carousel-item">
-              <img
-                src="../assets/LOTR/characterWordClouds/legolas.png"
-                class="d-block w-100"
-              />
-            </div>
-            <div class="carousel-item">
-              <img
-                src="../assets/LOTR/characterWordClouds/merry.png"
-                class="d-block w-100"
-              />
-            </div>
-            <div class="carousel-item">
-              <img
-                src="../assets/LOTR/characterWordClouds/pippin.png"
-                class="d-block w-100"
-              />
-            </div>
-            <div class="carousel-item">
-              <img
-                src="../assets/LOTR/characterWordClouds/sam.png"
-                class="d-block w-100"
-              />
-            </div>
-            <div class="carousel-item">
-              <img
-                src="../assets/LOTR/characterWordClouds/saruman.png"
-                class="d-block w-100"
-              />
-            </div>
-          </div>
-          <button
-            class="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="prev"
-          >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button
-            class="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="next"
-          >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-        </div>
+    </div>
   </div>
   <div class="row align-items-center p-3">
     <div class="col-md-12">
